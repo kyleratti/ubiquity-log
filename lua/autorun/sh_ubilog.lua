a7fabf50951d666df42dcc05a1380bff64da9e2c
Kyle <kyleratti@users.noreply.github.com>
2017-09-24 15:00:16 -0400

    Version 2
    Now using simploo

--- lua/autorun/sh_ubilog.lua
+++ lua/autorun/sh_ubilog.lua
@@ -1,43 +1,62 @@
-ubilog = ubilog or {}
+class "ubilog" {
+    const {
+        DEBUG = 1;
+        INFO = 2;
+        NOTICE = 3;
+        WARNING = 4;
+        ERROR = 5;
+        CRITICAL = 6;
+        ALERT = 7;
+        EMERGENCY = 8;
+    };
 
-local tblLogLevels = {
-    DEBUG = 1,
-    INFO = 2,
-    NOTICE = 3,
-    WARNING = 4,
-    ERROR = 5,
-    CRITICAL = 6,
-    ALERT = 7,
-    EMERGENCY = 8,
+    private {
+        m_strRealm = "";
+
+        doLog = function(self, strLogLevel, str, ...)
+            print(string.format("[%s] %s.%s: %s", os.date("%Y/%m/%d %H:%M:%S"), self:getRealm(), strLogLevel, string.format(str, unpack({...}))))
+        end;
+    };
+
+    public {
+        __construct = function(self, strRealm)
+            self.m_strRealm = strRealm
+        end;
+
+        getRealm = function(self)
+            return self.m_strRealm
+        end;
+
+        debug = function(self, str, ...)
+            self:doLog("DEBUG", str, ...)
+        end;
+
+        info = function(self, str, ...)
+            self:doLog("INFO", str, ...)
+        end;
+
+        notice = function(self, str, ...)
+            self:doLog("NOTICE", str, ...)
+        end;
+
+        warning = function(self, str, ...)
+            self:doLog("WARNING", str, ...)
+        end;
+
+        error = function(self, str, ...)
+            self:doLog("ERROR", str, ...)
+        end;
+
+        critical = function(self, str, ...)
+            self:doLog("CRITICAL", str, ...)
+        end;
+
+        alert = function(self, str, ...)
+            self:doLog("ALERT", str, ...)
+        end;
+
+        emergency = function(self, str, ...)
+            self:doLog("EMERGENCY", str, ...)
+        end;
+    };
 }
-
-local iCurrentLogLevel = tblLogLevels[1]
-
-local function doLog(strLogLevel, str)
-    print(string.format("[%s] %s: %s", os.date("%Y/%m/%d %H:%M:%S"), strLogLevel, str))
-end
-
-local function register(strName)
-    tblLogLevels[string.upper(strName)] = table.Count(tblLogLevels) + 1
-
-    ubilog[string.lower(strName)] = function(str, ...)
-        local strBuiltString = string.format(str, unpack({...}))
-
-        doLog(string.upper(strName), strBuiltString)
-    end
-end
-
-ubilog.getLevel = function()
-    return iCurrentLogLevel
-end
-
-ubilog.setLevel = function(iLogLevel)
-    iCurrentLogLevel = iLogLevel
-    ubilog.debug("Setting logging level to %s", iLogLevel)
-
-    return ubilog
-end
-
-for k,v in pairs({"debug", "info", "notice", "warning", "error", "critical", "alert", "emergency"}) do
-    register(v)
-end
